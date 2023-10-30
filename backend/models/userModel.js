f84bb04ec70d9baf91556b17fd83043de92d970f
Assaad <apple@apples-iMac.local>
2023-10-30 21:19:02 +0200

    Add User model schema in Mongoose

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose'; 
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: [true, "Enter your username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, "Enter your email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: [true, "Enter your password"]
+    },
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
