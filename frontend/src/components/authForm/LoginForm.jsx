e43b10340ff5265e23758def4dca03f478b1441b
Assaad <apple@apples-iMac.local>
2023-10-30 12:20:30 +0200

    Add Yup validation to LoginForm component

--- frontend/src/components/authForm/LoginForm.jsx
+++ frontend/src/components/authForm/LoginForm.jsx
@@ -1,13 +1,20 @@
 import {useFormik} from 'formik';
-import './authForm.css'
+import * as Yup from 'yup';
+
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Required"),
+  password: Yup.string().required("Required"),
+});
 
 const LoginForm = () => {
 
   const formik = useFormik({
-    initialValues:{
-      email: '',
-      password: '',
+    initialValues: {
+    email: '',
+    password: ''
     },
+    validationSchema,
     onSubmit: async (values) => {
       try{
         
@@ -18,27 +25,31 @@ const LoginForm = () => {
       }
     }
 
-  });
-  return (
-    <form onSubmit={formik.handleSubmit} className= "auth-form">
-      <input
-        type='email'
-        name='email'
-        placeholder='Email'
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
-      <input
-        type="password"
-        name='password'
-        placeholder='Password'
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
+});
 
-      
-      <button type='submit'>Login</button>
+return (
+    <form onSubmit={formik.handleSubmit} className="auth-form">
+    <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+    />
+    {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
+
+    <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+    />
+    {formik.touched.password && formik.errors.password ? <div className="error">{formik.errors.password}</div> : null}
+
+    <button type="submit">Login</button>
 
     </form>
   )
