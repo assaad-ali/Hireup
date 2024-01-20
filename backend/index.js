e1b243b8266c6075ae3eaa09492dbe792dbf23ec
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-20 11:33:27 +0200

    Add dotenv and cors for better setup
    Add dynamic PORT handling
    
    Add cors for cross origing requests handling

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import connectDB from "./config/db.js";
 import {authRouter} from "./routes/authRoutes.js"
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/auth', authRouter)
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000....")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}....`)
 })
