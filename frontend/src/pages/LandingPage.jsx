e8d1db426ae7d2e555efaef60a50133dae0dfc00
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-20 13:53:30 +0200

    Update LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,21 @@
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import AuthForm from "../components/authForm/AuthForm";
+import "./landingPage.css"
 
 const LandingPage = () => {
   return (
     <>
-        <header>
-          <Navbar />
-        </header>
+        <Navbar />
         <main>
-          <AuthForm />
+          <section className="hero-section">
+            <div className="right-container">
+            <h1>Find your next job</h1>
+            <p>With HireUp, you can find your next job in a few clicks. <br/> We have a wide range of jobs available for you to apply to.</p> 
+            </div>
+            <AuthForm />
+          </section>
+          
         </main>
         <Footer/>
     </>
