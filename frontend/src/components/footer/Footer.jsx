319c43a0522667bece0d61f66fc5bf5167046f23
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-20 13:39:50 +0200

    Update navigation in Footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
       <div className="footer-content">
         <p>HireUp © {year}</p>
         <div className="footer-links">
-          <a href="#">About</a>
-          <a href="#">Accessibility</a>
-          <a href="#">User Agreement</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Cookie Policy</a>
-          <a href="#">Copyright Policy</a>
-          <a href="#">Brand Policy</a>
-          <a href="#">Guest Controls</a>
-          <a href="#">Community Guidelines</a>
+          <a href="/">About</a>
+          <a href="/">Accessibility</a>
+          <a href="/">User Agreement</a>
+          <a href="/">Privacy Policy</a>
+          <a href="/">Cookie Policy</a>
+          <a href="/">Copyright Policy</a>
+          <a href="/">Brand Policy</a>
+          <a href="/">Guest Controls</a>
+          <a href="/">Community Guidelines</a>
         </div>
       </div>
     </footer>
