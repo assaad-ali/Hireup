179eb70dec76ca5bc0e546e7d27768ba7fb1ad6d
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-27 23:47:27 +0200

    Refactor sideBar for better navigation and UI
    - Replace react-pro-sidebar implementation with custom sidebar
    - Include react-icons
    - Update sidebar structure with links to Home, Friends, Jobs, Messages, Notifications, Settings, and Logout

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -1,20 +1,25 @@
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './sideBar.css';
+import { FiHome, FiArrowLeftCircle, FiArrowRightCircle, FiLogOut } from 'react-icons/fi';
+import { FaUserFriends, FaBriefcase } from 'react-icons/fa';
+import { RiMessage2Line, RiNotification3Line } from 'react-icons/ri';
+import { BiCog } from 'react-icons/bi';
 
 const SideBar = () => {
   return (
-    <>
-      <Sidebar>
-        <Menu>
-          <SubMenu>
-            <MenuItem> settings </MenuItem>
-            <MenuItem> privacy </MenuItem>
-          </SubMenu>
-          <MenuItem> netrworks </MenuItem>
-          <MenuItem> messages </MenuItem>
-        </Menu>
-      </Sidebar>
-    </>
-  );
-};
+    <div className='side-bar'>
+      <ul>
+        <li><Link to="/"><FiHome /> Home</Link></li>
+        <li><Link to="/friends"><FaUserFriends /> Friends</Link></li>
+        <li><Link to="/jobs"><FaBriefcase /> Jobs</Link></li>
+        <li><Link to="/messages"><RiMessage2Line /> Messages</Link></li>
+        <li><Link to="/notifications"><RiNotification3Line /> Notifications</Link></li>
+        <li><Link to="/settings"><BiCog /> Settings</Link></li>
+        <li><Link to="/logout"><FiLogOut /> Logout</Link></li>
+      </ul>
+    </div>
+  )
+}
 
 export default SideBar;
