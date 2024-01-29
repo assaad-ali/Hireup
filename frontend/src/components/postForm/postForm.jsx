f2919952a8d0d472718738f66ed34f9c0659468f
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-29 23:24:16 +0200

    Add state for the title field

--- frontend/src/components/postForm/postForm.jsx
+++ frontend/src/components/postForm/postForm.jsx
@@ -1,21 +1,27 @@
-import "./postForm.css"
-
+import "./postForm.css";
+import { useState } from "react";
 const PostForm = () => {
+  const [title, setTitle] = useState('');
   return (
     <section className="post-form-section">
       <form>
         <h2>Create a Job Post</h2>
         <div className="post-form-group">
           <label>Title:</label>
-          <input className="form-input"/>
+          <input
+            className="form-input"
+            type="text"
+            value={title}
+            onChange={({value})=> {setTitle(value) }}
+          />
         </div>
         <div className="post-form-group">
           <label>Description:</label>
-          <textarea className="post-form-textarea"/>
+          <textarea className="post-form-textarea" />
         </div>
         <div className="post-form-group">
           <label>Qualifications:</label>
-          <textarea className="post-form-textarea"/>
+          <textarea className="post-form-textarea" />
         </div>
         <button type="submit">Post Job</button>
       </form>
