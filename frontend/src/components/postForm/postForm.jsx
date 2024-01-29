ea6f29429f86f7caac0cc10d3149825a30b7b83c
Ali-Assaad <aliassaadtr@gmail.com>
2024-01-29 22:32:51 +0200

    Add job posting form to postForm component

--- frontend/src/components/postForm/postForm.jsx
+++ frontend/src/components/postForm/postForm.jsx
@@ -1,10 +1,26 @@
+import "./postForm.css"
 
-const postForm = () => {
+const PostForm = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <section className="post-form-section">
+      <form>
+        <h2>Create a Job Post</h2>
+        <div className="post-form-group">
+          <label>Title:</label>
+          <input className="form-input"/>
+        </div>
+        <div className="post-form-group">
+          <label>Description:</label>
+          <textarea className="post-form-textarea"/>
+        </div>
+        <div className="post-form-group">
+          <label>Qualifications:</label>
+          <textarea className="post-form-textarea"/>
+        </div>
+        <button type="submit">Post Job</button>
+      </form>
+    </section>
+  );
+};
 
-export default postForm
+export default PostForm;
